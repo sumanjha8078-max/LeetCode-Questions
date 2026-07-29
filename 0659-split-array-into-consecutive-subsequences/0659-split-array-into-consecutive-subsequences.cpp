ebcbe8d4ca036b6b5abe45c04d12e0551b29d659
Krishna Chandra Jha <sumanjha8078@gmail.com>
2026-07-30 00:39:07 +0530

    Time: 47 ms (9.67%), Space: 63.3 MB (10.33%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool is_true(vector<int>& use){
+        int n = use.size();
+        for(int i = 0 ; i < n ; i++){
+            if(use[i] == 0) continue;
+            int l = i, r = i + 1, val = use[i];
+            cout << val << "\n";
+            while(r < n && use[r] >= use[r-1]) r++;
+            cout << l << " " << r << "\n";
+            if(r - l < 3){
+                cout << "test2" << "\n";
+                return false;
+            } 
+            for(int j = l ; j < r ; j++) use[j]--;
+            i--;
+        }
+        return true;
+    }
+    bool isPossible(vector<int>& nums) {
+        map<int,int> mp;
+        for(const int& t : nums) mp[t]++;
+        vector<int> v;
+        for(const auto& p : mp) v.push_back(p.first);
+        int l = 0, r = 1;
+        int n = v.size();
+        set<pair<int,int>> st;
+        while(r < n){
+            while(r < n && v[r] == v[r-1] + 1) r++;
+            if(r - l < 3) return false;
+            st.insert({l, r});
+            l = r;
+            r++;
+        }
+        if(l != n){
+            cout << "test1" << "\n";
+            return false;
+        }
+        for(const auto& p : st){
+            cout << p.first << " " << p.second << "\n";
+            vector<int> use;
+            for(int i = p.first ; i < p.second ; i++) use.push_back(mp[v[i]]);
+            if(!is_true(use)) return false;
+        }
+        return true;
+    }
+};
